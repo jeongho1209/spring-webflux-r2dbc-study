0fcce0cd8f1d65c0477f2a6123b04726d7a461c6
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-08-26 11:14:48 +0900

    chore :: import 제거

--- src/main/kotlin/com/example/springwebflux/SpringWebFluxApplication.kt
+++ src/main/kotlin/com/example/springwebflux/SpringWebFluxApplication.kt
@@ -1,7 +1,6 @@
 package com.example.springwebflux
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
