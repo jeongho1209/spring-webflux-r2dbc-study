6050321beec806c8a090e9497fb904fb8fce0ff5
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-08-26 11:08:47 +0900

    fix :: db 의존성 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,12 +19,20 @@ repositories {
 }
 
 dependencies {
+    // r2dbc
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("io.asyncer:r2dbc-mysql:1.0.2")
+
+    // reactive
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+
+    // kotlin
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
 }
