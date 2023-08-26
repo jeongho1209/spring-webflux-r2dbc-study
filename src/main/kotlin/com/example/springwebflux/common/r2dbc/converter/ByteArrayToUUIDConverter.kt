0cd6242cf550a6cd91031d842a99f194efd5bff1
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-08-26 11:16:29 +0900

    add :: r2dbc config 추가

---
+++ src/main/kotlin/com/example/springwebflux/common/r2dbc/converter/ByteArrayToUUIDConverter.kt
@@ -0,0 +1,13 @@
+package com.example.springwebflux.common.r2dbc.converter
+
+import org.springframework.core.convert.converter.Converter
+import org.springframework.data.convert.ReadingConverter
+import java.nio.ByteBuffer
+import java.util.UUID
+
+@ReadingConverter
+class ByteArrayToUUIDConverter : Converter<ByteArray, UUID> {
+    override fun convert(source: ByteArray): UUID = ByteBuffer.wrap(source).run {
+        UUID(this.long, this.long)
+    }
+}
---
+++ src/main/kotlin/com/example/springwebflux/common/r2dbc/converter/UUIDToByteArrayConverter.kt
@@ -0,0 +1,15 @@
+package com.example.springwebflux.common.r2dbc.converter
+
+import org.springframework.core.convert.converter.Converter
+import org.springframework.data.convert.WritingConverter
+import java.nio.ByteBuffer
+import java.util.UUID
+
+@WritingConverter
+class UUIDToByteArrayConverter : Converter<UUID, ByteArray> {
+    override fun convert(source: UUID): ByteArray =
+        ByteBuffer.wrap(ByteArray(16)).apply {
+            putLong(source.mostSignificantBits)
+            putLong(source.leastSignificantBits)
+        }.array()
+}
