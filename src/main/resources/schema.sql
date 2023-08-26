b7edb0fb7ca6952dffbba31c1e4fa6c8fc5b3456
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-08-26 11:14:04 +0900

    fix :: 문법 오류 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,15 @@
-CREATE TABLE IF NOT EXIST `tbl_user`
+CREATE TABLE IF NOT EXISTS `tbl_user` (
     `id` BINARY(16) NOT NULL,
     `account_id` VARCHAR(20) NOT NULL,
     `password` CHAR(60) NOT NULL,
     `name` VARCHAR(10) NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
     CONSTRAINT `UNIQUE_account_id` UNIQUE(`account_id`)
 );
 
-CREATE TABLE IF NOT EXIST `tbl_feed`
+CREATE TABLE IF NOT EXISTS `tbl_feed` (
     `id` BINARY(16) NOT NULL,
+    `user_id` BINARY(16) NOT NULL,
     `title` VARCHAR(50) NOT NULL,
     `content` VARCHAR(1000) NOT NULL,
     PRIMARY KEY (`id`),
