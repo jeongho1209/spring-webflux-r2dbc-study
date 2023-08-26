aa650c7d0b0595cc62e1912ec82864adec8d51ea
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-08-26 11:15:13 +0900

    chore :: name 명시 제거

--- src/main/kotlin/com/example/springwebflux/domain/feed/domain/FeedEntity.kt
+++ src/main/kotlin/com/example/springwebflux/domain/feed/domain/FeedEntity.kt
@@ -4,11 +4,11 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import java.util.UUID
 
-@Table(name = "tbl_feed")
+@Table("tbl_feed")
 class FeedEntity(
     @Id
     val id: UUID = UUID.randomUUID(),
+    val userId: UUID,
     val title: String,
     val content: String,
-    val userId: UUID,
 )
