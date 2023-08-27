d13a4db86683f78a44f28f1a29b872fc63b2cde0
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-08-27 11:52:31 +0900

    chore :: @Id 제거

--- src/main/kotlin/com/example/springwebflux/domain/feed/domain/FeedEntity.kt
+++ src/main/kotlin/com/example/springwebflux/domain/feed/domain/FeedEntity.kt
@@ -1,12 +1,10 @@
 package com.example.springwebflux.domain.feed.domain
 
-import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import java.util.UUID
 
 @Table("tbl_feed")
 class FeedEntity(
-    @Id
     val id: UUID = UUID.randomUUID(),
     val userId: UUID,
     val title: String,
