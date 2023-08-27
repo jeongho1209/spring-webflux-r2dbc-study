bb37a5bed7fa02ce71d32d65470cced9a895bd94
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-08-27 11:51:07 +0900

    add :: BASE_PACKAGE 추가

--- src/main/kotlin/com/example/springwebflux/SpringWebFluxApplication.kt
+++ src/main/kotlin/com/example/springwebflux/SpringWebFluxApplication.kt
@@ -3,6 +3,8 @@ package com.example.springwebflux
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+internal const val BASE_PACKAGE = "com.example.springwebflux"
+
 @SpringBootApplication
 class SpringWebFluxApplication
 
